f3048ae6cffd92aa09ba6c48390a6b52c34ba545
gyan <1700010674@pku.edu.cn>
2020-05-27 18:14:16 +0800

    Add resnet20_featrue that can save feature vector in hidden layers

--- resnet.py
+++ resnet.py
@@ -1,9 +1,11 @@
 # network strucuture
 import torch.nn as nn
+import torch
+import torchvision as tv
 
 
 class residualBlock(nn.Module):
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, save_feature=False):
         super(residualBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channel, out_channel, 3, stride, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channel)
@@ -11,6 +13,8 @@ class residualBlock(nn.Module):
         self.conv2 = nn.Conv2d(out_channel, out_channel, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channel)
         self.downsample = downsample
+        self.feature = None
+        self.save_feature = save_feature
 
     def forward(self, x):
         y = x
@@ -23,6 +27,8 @@ class residualBlock(nn.Module):
         if self.downsample:
             y = self.downsample(y)
         x += y
+        if self.save_feature:
+            self.feature = x.data
         return x
 
 
@@ -65,3 +71,38 @@ class Resnet_20_CIFAR10(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+
+class Resnet_20_CIFAR10_feature(Resnet_20_CIFAR10):
+    """Resnet_20 model saving the feature in hidden layers"""
+    def __init__(self):
+        super(Resnet_20_CIFAR10_feature, self).__init__()
+        for i in range(3):
+            self.layer1[i].save_feature = True
+            self.layer2[i].save_feature = True
+            self.layer3[i].save_feature = True
+
+    def get_feature(self, i):
+        """Get the feature vector after ith block"""
+        assert isinstance(i, int) and i >= 0 and i < 9
+        m, n = i // 3, i - 3 * (i // 3)
+        layer = eval("self.layer" + str(m+1))
+        return layer[n].feature
+
+
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    batchsize = 500
+    testset = tv.datasets.CIFAR10("data/", train=False,
+                                  transform=tv.transforms.ToTensor(), download=True)
+    testloader = torch.utils.data.DataLoader(testset, batchsize, shuffle=False)
+    model = Resnet_20_CIFAR10_feature().to(device)
+    model.load_state_dict(torch.load("testmodel_resnet_cifar.ckpt", map_location=device))
+    print("Successfully load weights")
+    model.eval()
+    for x, y in testloader:
+        x = x.to(device)
+        output = model(x)
+        for i in range(9):
+            print(model.get_feature(i).shape)
+        break
